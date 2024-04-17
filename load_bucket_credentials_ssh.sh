7cb14665794eed88779a4f89fdc2d3f6cff11229
avidalto <avidalto@hawk.iit.edu>
2024-04-17 08:22:01 -0500

    Add script to load bucket credentials through ssh

---
+++ load_bucket_credentials_ssh.sh
@@ -0,0 +1,8 @@
+# Load credentials
+eval $(ssh ${resource_ssh_usercontainer_options} usercontainer ${pw_job_dir}/workflow-utils/bucket_token_generator.py --bucket_id ${dcs_bucket_id} --token_format text)
+
+# Check if dcs_model_file is empty
+if [ -z "${BUCKET_NAME}" ]; then
+    echo "Error: BUCKET_NAME variable is empty"
+    exit 1
+fi
